b356a9d71f4fdbe9d349d084f2daab82cc169a09
Per Øyvind Karlsen <proyvind@moondrake.org>
2017-04-01 21:18:01 +0200

    implement a generic plugin type interface for embeddeding of external interpreters

---
+++ rpmio/rpminterp.c
@@ -0,0 +1,81 @@
+#include "system.h"
+
+#include <dlfcn.h>
+#include <rpm/rpmlog.h>
+
+#include <rpm/rpmio.h>
+#include <rpm/rpmmacro.h>
+#include <rpm/rpminterp.h>
+
+#include "debug.h"
+
+int _rpminterp_debug = 0;
+
+static void rpminterpFree(int argc, void *p) {
+    rpminterp interp = p;
+    interp->free();
+    if (dlclose(interp->h))
+	rpmlog(RPMLOG_WARNING, "Error closing rpminterp \"%s\": %s", interp->name, dlerror());
+}
+
+rpminterp rpminterpLoad(const char* name, const char *modpath) {
+    char buf[64];
+    struct rpminterp_s *interp = NULL;
+    void *h = NULL;
+    Dl_info info;
+    ARGV_t files = NULL;
+    rpmRC rc = RPMRC_FAIL;
+    rpminterpFlag flags = RPMINTERP_DEFAULT;
+
+    snprintf(buf, sizeof(buf), "%%_rpminterp_%s_flags", name);
+
+    flags = rpmExpandNumeric(buf);
+
+    snprintf(buf, sizeof(buf), "rpminterp_%s", name);
+
+    if ((interp = dlsym(RTLD_DEFAULT, buf)))
+	return interp;
+
+    if (_rpminterp_debug)
+	rpmlog(RPMLOG_DEBUG, " %8s (modpath) %s\n", __func__, modpath);
+
+    if (rpmGlob(modpath, NULL, &files) != 0) {
+	rpmlog(RPMLOG_WARNING, "\"%s\" does not exist, "
+		"embedded %s will not be available\n",
+		modpath, name);
+    } else if (!(h = dlopen((modpath = *files), RTLD_LAZY|RTLD_GLOBAL))) {
+	rpmlog(RPMLOG_WARNING, "Unable to open \"%s\" (%s), "
+		"embedded %s will not be available\n",
+		modpath, dlerror(), name);
+    } else if (!(interp = dlsym(h, buf))) {
+	rpmlog(RPMLOG_WARNING, "Opened library \"%s\" is incompatible (%s), "
+		"embedded %s will not be available\n",
+		modpath, dlerror(), name);
+    } else if (dladdr(interp->init, &info) && strcmp(modpath, info.dli_fname)) {
+	rpmlog(RPMLOG_WARNING, "\"%s\" lacks %s interpreter support, "
+		"embedding of will not be available\n",
+		modpath, name);
+    } else if ((rc = interp->init(NULL, flags)) != RPMRC_OK) {
+	rpmlog(RPMLOG_WARNING, "%s->init() != RPMRC_OK: %d\n",
+		buf, rc);
+	rpminterpFree(0, interp);
+    }
+
+    if (_rpminterp_debug)
+	rpmlog(RPMLOG_DEBUG, " %8s (_rpminterp_%s_modpath, %d) %s\n", __func__, name, rc, modpath);
+
+    if (h) {
+	if (rc == RPMRC_OK) {
+	    on_exit(rpminterpFree, interp);
+	    interp->h = h;
+	} else if (dlclose(h)) {
+	    rpmlog(RPMLOG_WARNING, "Error closing library \"%s\": %s", modpath,
+		    dlerror());
+	}
+    }
+
+    argvFree(files);
+
+    return interp;
+}
+
---
+++ rpmio/rpminterp.h
@@ -0,0 +1,38 @@
+#ifndef _H_INTERP_
+#define _H_INTERP_
+
+extern int _rpminterp_debug;
+
+/** \ingroup rpminterp
+ * Initialization flags for init().
+ */
+typedef	enum rpminterpFlag_e {
+    RPMINTERP_NO_INIT    	= 1<<30,
+    RPMINTERP_NO_IO_REDIR	= 1<<29,
+    RPMINTERP_DEFAULT 		= 0
+} rpminterpFlag;
+
+struct rpminterp_s {
+	const char *name;
+	rpmRC (*init) (ARGV_t *av, rpminterpFlag flags);
+	void (*free) (void);
+	rpmRC (*run) (const char * str, char ** resultp);
+	void *h;
+};
+
+typedef const struct rpminterp_s * rpminterp;
+
+#define rpminterpInit(name, init, free, run) \
+	struct rpminterp_s rpminterp_ ## name = { #name, init, free, run, NULL}
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+rpminterp rpminterpLoad(const char* name, const char *modpath);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _H_INTERP_ */
